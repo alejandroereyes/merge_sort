e299c3e1a17f62d201c278f529becc21ec21b117
Alex Reyes <alejandroereyes@gmail.com>
2016-08-03 09:18:56 -0500

    completed version of recursive merge_sort

--- sort.rb
+++ sort.rb
@@ -3,24 +3,29 @@ module Sort
   # merge_sort executes the merge sort algorithm recursively
   module_function
 
-  def merge_sort(init_arr)
-    # Divide: arr into half, or half+1 for one of them if arr.length.odd?
-    half_length = init_arr.length/2
-    multi_arr   = [init_arr[0...half_length], init_arr[half_length..-1]]
+  def merge_sort(init_arr, which_arr: "init_arr")
+    # base case
+    return init_arr if init_arr.length == 1
 
-    if multi_arr.all? { |inner_arr| inner_arr.length == 1 }
-      return multi_arr
-    end
+    # divide
+    half_length = (init_arr.length - 1) / 2
+    arr_one     = init_arr[0..half_length]
+    arr_two     = init_arr[half_length + 1..-1]
+    # multi_arr   = [arr_one, arr_two] # uncomment to see progression
+    # puts "#{which_arr}: #{multi_arr.inspect}" # uncomment to see progression
 
-    multi_arr.each_with_object([]) do |elem, arr|
-      arr << elem             if elem.length == 1
-      arr << merge_sort(elem) if elem.length != 1
-    end
-
-    # Combine: Merge the two sorted smaller arrays into a single sorted array
+    # Combine: merge & sort the two smaller arrays into a single sorted array
+    recur_one = merge_sort(arr_one, which_arr: 'arr_one')
+    recur_two = merge_sort(arr_two, which_arr: 'arr_two')
+    # puts " recur_one: #{recur_one.inspect}\n recur_two: #{recur_two.inspect}" # uncomment to see progression
+    new_arr = (recur_one + recur_two).sort
+    # puts "new arr: #{new_arr.inspect}\n" # uncomment to see progression
+    new_arr
   end
 end
 
 puts "Sort.merge_sort [1, 2, 3] # => #{Sort.merge_sort [3, 1, 2]}"
+puts ""
+puts "Sort.merge_sort [14, 7, 3, 12, 9, 11, 6, 2] # => #{Sort.merge_sort [14, 7, 3, 12, 9, 11, 6, 2]}"
 puts ""
 puts "Sort.merge_sort [1, 2, 3] # => #{Sort.merge_sort [3, 1, 4, 2]}"
